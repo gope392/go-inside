02ae4f077f6686fdb0a9af55c8a12eeab6747201
gope <goperezpantoja@gmail.com>
2024-04-11 12:31:41 -0400

    test2

--- src/goinside.py
+++ src/goinside.py
@@ -1,7 +1,16 @@
 from openpyxl import load_workbook
 import sys
 
+#import xlsx file
 #xls_file = input('Enter Excel file path')
 xls_file = sys.argv[1]
 
-wbook = load_workbook(filename=xls_file)
+wbook = load_workbook(xls_file)
+
+print('Please Select Worksheet:')
+for name in wbook.sheetnames:
+    print(name)
+
+def menu():
+    print('select:')
+    
