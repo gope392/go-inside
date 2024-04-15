5b1834a4c4f24393d5fcd9781912b98782e18f33
gope <goperezpantoja@gmail.com>
2024-04-15 16:25:57 -0400

    column review

--- src/goinside.py
+++ src/goinside.py
@@ -4,10 +4,23 @@ import sys
 #import xlsx file
 #xls_file = input('Enter Excel file path')
 xls_file = sys.argv[1]
+workbook = load_workbook(xls_file)
+worksheets = workbook.worksheets
 
-wbook = load_workbook(xls_file)
+for worksheet in worksheets:
+    workbook.active = worksheet
+    print(f'El nombre del worksheet activo es: {workbook.active.title}')
+    columns = workbook.active.iter_cols(max_row=1, values_only=True)
 
-print('Worksheets:')
-for name in wbook.sheetnames:
-    print(name)
+
+#Identify worksheets
+#wsheets = wbook.worksheets
+
+#for wsheet in wsheets:
+#    print 
+#    print('max row: ' + whseet.max_row)
+
+#print('Worksheets:')
+#for name in wbook.sheetnames:
+#    print(name)
 
